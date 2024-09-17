ef71fa3bf079b327b2a5364edb693c7832ad14c6
T-Coder0102 <t.joldasbayev@newuu.uz>
2024-09-17 10:51:26 +0500

    done commit1

---
+++ problem10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>;
+using namespace std;
+int main() {
+    float price1,weight1,price2,weight2;
+    cout<<"Enter weight and price for package1: ";
+    cin>>weight1>>price1;
+    cout<<"Enter weight and price for package2: \n";
+    cin>>weight2>>price2;
+    if(weight1/price1<weight2/price2) {
+
+    }
+}
