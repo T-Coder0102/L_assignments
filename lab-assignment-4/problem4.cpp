597066d9269a905a93462c4f49da572bca3804d8
Timur <timurjoldasbayev03@gmail.com>
2024-09-28 18:32:07 +0500

    lab-assignment testrun

--- lab-assignment-4/problem4.cpp
+++ lab-assignment-4/problem4.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 int main()
 {
-    int n,x;
+    int n,x,f=1,temp=2;
     cout<<"Enter two integers: ";
     cin>>n>>x;
-    
+        if(n==1 || n==2)
+        {
+            cout<<f;
+        }else
+        {
+            while(true)
+            {
+                if(temp==n)
+                {
+                    cout<<f;
+                    break;
+                }
+                if(temp==x*f-(x-3))
+                {
+                    f++;
+                }temp++;
+            }
+        }
 }
--- lab-assignment-4/problem5.cpp
+++ lab-assignment-4/problem5.cpp
@@ -1,3 +1,23 @@
-//
-// Created by timur on 9/27/2024.
-//
+#include <iostream>;
+using namespace std;
+int main()
+{
+    int n,a=1,r=0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    while(true)
+    {
+        if(a%3!=0 && (a-3)%10!=0)
+        {
+            r++;
+            if(r==n){
+                cout<<a<<endl;
+                break;
+            }
+            a++;
+        }else
+        {
+            a++;
+        }
+    }
+}
